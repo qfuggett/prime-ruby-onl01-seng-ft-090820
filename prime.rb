3570f2c1291d51618334ec481289d89bd9f59ad6
QueenTesa Fuggett <qfuggett@gmail.com>
2020-09-20 21:24:08 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,18 @@
-# Add  code here!
+require 'pry'
+
+def prime?(number)
+if number < 2 
+  return false
+
+else
+  n = 2
+  while n <= number / 2
+    return false if number % n == 0
+    n += 1
+  end
+
+  return true
+
+end
+
+end
