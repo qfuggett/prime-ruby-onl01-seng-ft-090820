c5dcb2950e9f8d9e0310998c26d7d5aaf4a55423
QueenTesa Fuggett <qfuggett@gmail.com>
2020-09-20 21:06:00 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -2,11 +2,11 @@ require 'pry'
 
 def prime?(number)
 binding.pry
-if number == number / -1
+if number <= 1 || number == 0 || number == 1
   return false
 
-else
-  n = 2
+elsif
+  (2..value - 1).each do |i|
   while n <= number
     return false if number % n == 0
     n += 1
