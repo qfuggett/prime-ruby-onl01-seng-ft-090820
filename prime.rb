c8f281336c745cf994ddb5e4907e806ea1c6746f
QueenTesa Fuggett <qfuggett@gmail.com>
2020-09-20 21:06:59 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -6,7 +6,7 @@ if number <= 1 || number == 0 || number == 1
   return false
 
 elsif
-  (2..value - 1).each do |i|
+  (2..value -1).each do |i|         #counting from 
   while n <= number
     return false if number % n == 0
     n += 1
