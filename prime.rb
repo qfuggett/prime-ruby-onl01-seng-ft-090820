650dee2325bf4934443a35b904fe11ff99df5cbe
QueenTesa Fuggett <qfuggett@gmail.com>
2020-09-20 21:00:57 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,1 +1,16 @@
-# Add  code here!
+def prime?(number)
+if number == number / -1
+  return false
+
+else
+  n = 2
+  while n <= number
+    return false if number % n == 0
+    n += 1
+  end
+
+  return true
+
+end
+
+end
